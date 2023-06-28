b4b2bf729f0570b8c1c997a0a0db6f8e7bc15a0b
Denis <58222550+denisroghelia@users.noreply.github.com>
2023-06-28 19:17:07 +0530

    Add files via upload

--- blogs/models.py
+++ blogs/models.py
@@ -4,12 +4,17 @@ class User(models.Model):
 	id    = models.AutoField(primary_key=True)
 	fname = models.CharField(max_length=20)
 	lname = models.CharField(max_length=20)
+	username = models.CharField(max_length=20)
 	email = models.EmailField()
 	about = models.TextField()
 	password = models.CharField(max_length=16)
 
 	def get_user_by_id(_id):
 		user = User.objects.get(id=_id)
+		return user
+
+	def get_user_by_username(_username):
+		user = User.objects.get(username=_username)
 		return user
 
 	def __str__(self):
@@ -26,6 +31,10 @@ class Blog(models.Model):
 	def get_blog_by_id(_id):
 		blog = Blog.objects.get(id=_id)
 		return blog
+
+	def get_blogs_by_user(_user):
+		blogs = Blog.objects.filter(user=_user)
+		return blogs
 
 	def get_latest_blogs():
 		return Blog.objects.order_by('-pub_date')[:12]
--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render, HttpResponse
-from .models import Blog
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Blog, User
 
 # Create your views here.
 def index(request):
-	return render(request, "home.html")
+	if request.COOKIES.get('user') and request.COOKIES.get('user') != "":
+		user = User.get_user_by_id(request.COOKIES['user'])
+		context = {
+			'user': User.get_user_by_id(request.COOKIES['user']),
+			'blogs': Blog.get_blogs_by_user(user),
+		}
+		return render(request, "dashboard.html", context)
+	else:
+		return render(request, "home.html")
 
 def blogs(request):
 	context = {
@@ -18,7 +26,31 @@ def blog(request, id):
 	return render(request, "blog.html", context)
 
 def login(request):
-	return HttpResponse("Login Page")
+	return render(request, "login.html")
+
+def login_auth(request):
+	username = request.GET["username"]
+	password = request.GET["password"]
+
+	try:
+		user = User.get_user_by_username(username)
+	except:
+		return HttpResponse("Incorrect Username")
+
+	if user.password == password:
+		response = redirect('index')
+		response.set_cookie('user', user.id)
+
+	else:
+		response = HttpResponse("Incorrect Password")
+
+	return response
+
+def logout(request):
+	response = redirect("index")
+
+	response.set_cookie('user', "")
+	return response
 
 def create(request):
 	return HttpResponse("Create Blog")
