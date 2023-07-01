c5fe45605f556564f60ee9035e9fb25711abbe7c
Denis <58222550+denisroghelia@users.noreply.github.com>
2023-07-01 11:33:06 +0530

    Add files via upload

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
 	path('', views.index, name="index"),
 	path('blogs/', views.blogs, name="blogs"),
@@ -8,5 +11,6 @@ urlpatterns = [
 	path('login/', views.login, name="login"),
 	path('logout/', views.logout, name="logout"),
 	path('create/', views.create, name="create"),
+	path('post', views.post, name="post"),
 	path('login-auth/', views.login_auth, name="login-auth"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -2,55 +2,78 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import Blog, User
 
 # Create your views here.
+
+
 def index(request):
-	if request.COOKIES.get('user') and request.COOKIES.get('user') != "":
-		user = User.get_user_by_id(request.COOKIES['user'])
-		context = {
-			'user': User.get_user_by_id(request.COOKIES['user']),
-			'blogs': Blog.get_blogs_by_user(user),
-		}
-		return render(request, "dashboard.html", context)
-	else:
-		return render(request, "home.html")
+    if request.COOKIES.get('user') and request.COOKIES.get('user') != "":
+        user = User.get_user_by_id(request.COOKIES['user'])
+        context = {
+            'user': User.get_user_by_id(request.COOKIES['user']),
+            'blogs': Blog.get_blogs_by_user(user),
+        }
+        return render(request, "dashboard.html", context)
+    else:
+        return render(request, "home.html")
+
 
 def blogs(request):
-	context = {
-		'blogs': Blog.get_latest_blogs(),
-	}
-	return render(request, "blogs.html", context)
+    context = {
+        'blogs': Blog.get_latest_blogs(),
+    }
+    return render(request, "blogs.html", context)
+
 
 def blog(request, id):
-	context = {
-		'blog': Blog.get_blog_by_id(id),
-	}
-	return render(request, "blog.html", context)
+    context = {
+        'blog': Blog.get_blog_by_id(id),
+    }
+    return render(request, "blog.html", context)
+
 
 def login(request):
-	return render(request, "login.html")
+    return render(request, "login.html")
+
 
 def login_auth(request):
-	username = request.GET["username"]
-	password = request.GET["password"]
+    username = request.GET["username"]
+    password = request.GET["password"]
 
-	try:
-		user = User.get_user_by_username(username)
-	except:
-		return HttpResponse("Incorrect Username")
+    try:
+        user = User.get_user_by_username(username)
+    except:
+        return HttpResponse("Incorrect Username")
 
-	if user.password == password:
-		response = redirect('index')
-		response.set_cookie('user', user.id)
+    if user.password == password:
+        response = redirect('index')
+        response.set_cookie('user', user.id)
 
-	else:
-		response = HttpResponse("Incorrect Password")
+    else:
+        response = HttpResponse("Incorrect Password")
 
-	return response
+    return response
+
 
 def logout(request):
-	response = redirect("index")
+    response = redirect("index")
 
-	response.set_cookie('user', "")
-	return response
+    response.set_cookie('user', "")
+    return response
+
 
 def create(request):
-	return HttpResponse("Create Blog")
+    return render(request, "create.html")
+
+
+def post(request):
+    try:
+        title = request.POST['title']
+        user = User.get_user_by_id(request.COOKIES.get('user'))
+        cover = request.POST['cover']
+        body = request.POST['body']
+
+        blog = Blog(title=title, cover=cover, user=user, body=body)
+        blog.save()
+        
+        return redirect('index')
+    except:
+        return HttpResponse("Error creating blog, try login again")
