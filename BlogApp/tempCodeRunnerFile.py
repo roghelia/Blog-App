5f50c2e1bfeda0534e9adb7f0592aa44dab2a8fc
Denis <58222550+denisroghelia@users.noreply.github.com>
2023-08-07 16:32:58 +0530

    Update tempCodeRunnerFile.py

--- BlogApp/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-def print_grid(rows, cols):
-#     for i in range(1, rows + 1):
-#         for j in range(1, cols + 1):
-#             num = (j - 1) * rows + i
-#             print(num, end="\t")
-#         print()
